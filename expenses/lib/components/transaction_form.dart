871a8da0bb256e6ecf8f608cc2602d02c41a7f43
francisco franco <franciscofrancodev@gmail.com>
2023-09-04 21:45:14 -0300

    adding modal to transaction

--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -1,58 +1,68 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final void Function(String title, double value) onSubmit;
-  TransactionForm(this.onSubmit);
+class TransactionForm extends StatefulWidget {
+  final void Function(String, double) onSubmit;
+
+  const TransactionForm(this.onSubmit, {Key? key}) : super(key: key);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+  final valueController = TextEditingController();
+
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
+    widget.onSubmit(title, value);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final titleController = TextEditingController();
-    final valueController = TextEditingController();
-
-    _submitForm() {
-      final title = titleController.text;
-      final value = double.tryParse(valueController.text) ?? 0.0;
-
-      if (title.isEmpty || value <= 0) {
-        return;
-      }
-
-      onSubmit(title, value);
-    }
-
     return Card(
       elevation: 5,
       child: Padding(
         padding: const EdgeInsets.all(10),
         child: Column(
-          children: <Widget>[
+          children: [
             TextField(
               controller: titleController,
               onSubmitted: (_) => _submitForm(),
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Título',
               ),
             ),
             TextField(
               controller: valueController,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
               onSubmitted: (_) => _submitForm(),
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Valor (R\$)',
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
-              children: [
+              children: <Widget>[
                 TextButton(
-                  onPressed: _submitForm,
-                  style: TextButton.styleFrom(
-                    primary: Colors.purple,
+                  child: const Text(
+                    'Nova Transação',
+                    style: TextStyle(
+                      color: Colors.purple,
+                    ),
                   ),
-                  child: Text('Nova transação'),
-                ),
+                  onPressed: _submitForm,
+                )
               ],
-            )
+            ),
           ],
         ),
       ),
