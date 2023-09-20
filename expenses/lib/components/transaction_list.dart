17ab785c1679987624e18d1144470f1b287815c9
francisco franco <franciscofrancodev@gmail.com>
2023-09-20 19:29:48 -0300

    Refactor: Extracting components

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -1,14 +1,13 @@
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'transaction_item.dart';
+import './transaction_item.dart';
+import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final void Function(String) onRemove;
 
-  TransactionList(this.transactions, this.onRemove);
-
-  get _deleteTransaction => null;
+  const TransactionList(this.transactions, this.onRemove, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +15,14 @@ class TransactionList extends StatelessWidget {
         ? LayoutBuilder(
             builder: (ctx, constraints) {
               return Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                  ),
+                children: [
+                  const SizedBox(height: 20),
                   Text(
                     'Nenhuma Transação Cadastrada!',
-                    style: Theme.of(ctx).textTheme.headline6,
+                    style: Theme.of(context).textTheme.headline6,
                   ),
+                  const SizedBox(height: 20),
                   SizedBox(
-                    height: 20,
-                  ),
-                  Container(
                     height: constraints.maxHeight * 0.6,
                     child: Image.asset(
                       'assets/images/waiting.png',
@@ -40,7 +35,7 @@ class TransactionList extends StatelessWidget {
           )
         : ListView.builder(
             itemCount: transactions.length,
-            itemBuilder: (context, index) {
+            itemBuilder: (ctx, index) {
               final tr = transactions[index];
               return TransactionItem(
                 tr: tr,
